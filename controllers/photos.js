a80ed4455aab482bcac6b37a135f9dd699767a19
Hannah Werman <hanna.w28@gmail.com>
2018-06-25 22:28:35 -0500

    created a photo show page. will need to reformat into a SPA but its a working start

--- controllers/photos.js
+++ controllers/photos.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Photos = require('../models/photo');
 const User = require('../models/user');
 
+// add a new photo route
 router.get('/new', (req, res, next) => {
   res.render('photos/new.ejs')
 })
@@ -27,6 +28,44 @@ router.post('/', async (req, res, next) => {
     }
 
   } catch (err) {
+    next(err)
+  }
+})
+
+// a photo show page
+router.get('/:id', async(req,res, next) => {
+  console.log(req.body.id)
+  const foundUser = await User.findOne({username: req.session.username})
+  const foundPhoto = await Photos.findOne({_id: req.params.id})
+  console.log(foundPhoto)
+  res.render('photos/show.ejs', {
+    id: req.body.id,
+    user: foundUser,
+    photo: foundPhoto.url,
+    access_token: req.session.access_token,
+    refresh_token: req.session.refresh_token,
+  })
+})
+
+// edit photo route
+router.get('/edit', async(req, res, next) => {
+  try {
+    const user = await Users.findOne({username: req.session.username});
+
+    if (user) {
+      if (user.photos) {
+        res.render('photos/edit.ejs', {
+          user: user
+        })
+      } else {
+        req.session.message = "You do not have any photos. Please add content to your page before trying to edit."
+        res.redirect('/users/' + user._id)
+      }
+    } else {
+      req.session.message = "You must be logged in to add or edit content."
+      res.redirect('/')
+    }
+  } catch(err) {
     next(err)
   }
 })
